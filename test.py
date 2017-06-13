dc1f0980d777a9712b8f9622d1fe64fce408d288
Scott Huffman <shuffman@wcu.edu>
2017-06-12 20:24:30 -0400

    all in for dataframes

--- test.py
+++ test.py
@@ -9,13 +9,33 @@ conn = sqlite3.connect(dbfile)
 c = conn.cursor()
 rooms = []
 catid = []
-c.execute('select catid,room from Bot')
+CAS = []
+d = {}
+c.execute('select catid,room,CAS from Bot')
 tmp1 = c.fetchall()
 for i in range(len(tmp1)):
     catid.append(tmp1[i][0])
     rooms.append(tmp1[i][1])
+    CAS.append(tmp1[i][2])
+    tmp2 = []
+    for j in range(1,len(tmp1[i])):
+        tmp2.append(tmp1[i][j])
+    d[tmp1[i][0]] = tmp2
     conn.commit()
 c.close()
 
-df= pd.Series(rooms,index=catid)
+
+
+df= pd.DataFrame.from_dict(d,orient='index')
+df.rename(columns={0:'room',1:'CAS'},inplace=True)
+df.index.name = 'CATID'
+
+
+roomsdf = df.room.unique()
+roomslist = roomsdf.tolist()
+#filter rooms that should not be rooms
+tmp = [ x for x in roomslist if 'retire' not in x ]
+tmp = [ x for x in tmp if 'combine' not in x ]
+tmp = [ x for x in tmp if 'neu' not in x ]
+rooms = tmp
 
